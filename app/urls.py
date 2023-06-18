ff9f3bb601b9716cdcdd69dc86ab2138384d79fa
Sergey <ssnovak@stud.kpfu.ru>
2023-06-18 20:58:05 +0300

    + access/refresh tokens

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import RegistrationView, LoginView, LogoutView
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name='registration'),
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
