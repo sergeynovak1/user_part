c80aa0807a229a9c0a2f60ffa67f82b50ef93264
Sergey <ssnovak@stud.kpfu.ru>
2023-06-18 16:54:48 +0300

    + "Cookie: csrftoken=SxHMzHsRj5mqXwnDZzmk7tv9fsV03w30; sessionid=huefk0onj5k4as9orrisu9pohpbstafm" in headers of network

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 from .views import RegistrationView, LoginView, LogoutView
@@ -6,4 +6,5 @@ urlpatterns = [
     path("registration/", RegistrationView.as_view(), name='registration'),
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
+    path('drf-auth/', include('rest_framework.urls'))
 ]
