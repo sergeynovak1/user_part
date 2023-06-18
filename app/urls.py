46bba1bb70e0e46ea19b6cacd1632600af18bae4
Sergey <ssnovak@stud.kpfu.ru>
2023-06-18 20:17:40 +0300

    + auth/users => create auth/token/login => login return token

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include,re_path
 from django.views.generic import RedirectView
 
 from .views import RegistrationView, LoginView, LogoutView
@@ -6,4 +6,6 @@ urlpatterns = [
     path("registration/", RegistrationView.as_view(), name='registration'),
     path("login/", LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
+    path('auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
